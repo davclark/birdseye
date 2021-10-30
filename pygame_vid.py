ebd602c26e98db6f2a7d91421d9bb8308c5b3b00
Dav Clark <dav@bead.glass>
2021-10-30 13:09:06 -0700

    Exploration and refactor pygame_vid in prep for threshold logic

--- pygame_vid.py
+++ pygame_vid.py
@@ -1,26 +1,58 @@
 #!/usr/bin/env python3.7
 import cv2
 import pygame
+import numpy as np
 
-'''Play the video at 60 fps irrespective of video'''
+'''Play the thresholded video'''
 
-cap = cv2.VideoCapture('sample_media/bird flying led sample.mp4')
-success, img = cap.read()
-fps = cap.get(cv2.CAP_PROP_FPS)
-shape = img.shape[1::-1]
+def thresholded_component(img, lower, upper, component_idx):
+    '''Take one component / layer / color and return a map of values between upper and lower'''
+    component = img[:,:,component_idx]
+    _, lower_thresh = cv2.threshold(component, lower, 1, cv2.THRESH_BINARY)
+    _, upper_thresh = cv2.threshold(component, upper, 1, cv2.THRESH_BINARY_INV)
+    return lower_thresh * upper_thresh
 
-wn = pygame.display.set_mode(shape)
-clock = pygame.time.Clock()
+# Constants to access YCrCb colorspace
+Y = 0
+Cr = 1
+Cb = 2
 
-while success:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            success = False
-    vid_surface = pygame.image.frombuffer(img.tobytes(), shape, "BGR")
+
+def draw():
+    success, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+
+    rgb = np.repeat(gray[:,:,np.newaxis], 3, axis=2)
+
+    # vid_surface = pygame.image.frombuffer(img.tobytes(), shape, "BGR")
+    vid_surface = pygame.image.frombuffer(rgb, shape, "BGR")
     wn.blit(vid_surface, (0, 0))
     pygame.display.update()
 
-    success, img = cap.read()
     clock.tick(fps)
 
-pygame.quit()
+    return success
+
+
+if __name__ == '__main__':
+    # This is like setup() in processing
+    cap = cv2.VideoCapture('sample_media/bird flying led sample.mp4')
+    success, img = cap.read()
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    shape = img.shape[1::-1]
+
+    wn = pygame.display.set_mode(shape)
+    clock = pygame.time.Clock()
+
+    # This loop will keep calling our draw function until we quit (e.g., close the windoww), or
+    # if capture fails (for now if we reach the end of the video)
+    while success:
+        success = draw()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                success = False
+
+    pygame.quit()
